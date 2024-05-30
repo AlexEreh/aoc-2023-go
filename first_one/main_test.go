1a668210b7a84ff688938e68c81abc3146856c3e
Alexander Ereshkin <xcode111@mail.ru>
2024-05-31 02:57:18 +0300

    Increase code coverage in first test, fix expected/actual order in tests.

--- first_one/main_test.go
+++ first_one/main_test.go
@@ -9,21 +9,34 @@ func TestReverse(t *testing.T) {
 	t.Run("First test", func(t *testing.T) {
 		got := Reverse("amogus")
 		want := "sugoma"
-		require.Equal(t, got, want)
+		require.Equal(t, want, got)
 	})
 	t.Run("Second test", func(t *testing.T) {
 		got := Reverse("four")
 		want := "ruof"
-		require.Equal(t, got, want)
+		require.Equal(t, want, got)
 	})
 	t.Run("Third test", func(t *testing.T) {
 		got := Reverse("")
 		want := ""
-		require.Equal(t, got, want)
+		require.Equal(t, want, got)
 	})
 	t.Run("Fourth test", func(t *testing.T) {
 		got := Reverse("lol")
 		want := "lol"
-		require.Equal(t, got, want)
+		require.Equal(t, want, got)
+	})
+}
+
+func TestLineCalibrationNumber(t *testing.T) {
+	t.Run("First test", func(t *testing.T) {
+		got := LineCalibrationNumber("lol1lol")
+		want := 11
+		require.Equal(t, want, got)
+	})
+	t.Run("Second test", func(t *testing.T) {
+		got := LineCalibrationNumber("ac1bg2ffer")
+		want := 12
+		require.Equal(t, want, got)
 	})
 }
